f7ba90342f74ec3883953515c0bf16d5b46a37a9
Matheus Orestes <mherreirao@gmail.com>
2026-01-28 12:05:08 -0300

    Criando o projeto

---
+++ src/main/java/com/web_services/springwebservices/SpringwebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.web_services.springwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web_services/springwebservices/SpringwebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_services.springwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
