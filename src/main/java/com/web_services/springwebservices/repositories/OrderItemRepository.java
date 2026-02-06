275bfc6d05c9c2baa5971a7ed8ba6016ff918e39
Matheus Orestes <mherreirao@gmail.com>
2026-02-06 15:50:52 -0300

    feat: Enhance OrderItem and Product entities with relationships and data retrieval methods

--- src/main/java/com/web_services/springwebservices/repositories/OrderItemRepository.java
+++ src/main/java/com/web_services/springwebservices/repositories/OrderItemRepository.java
@@ -1,12 +1,13 @@
 package com.web_services.springwebservices.repositories;
 
 import com.web_services.springwebservices.entities.OrderItem;
+import com.web_services.springwebservices.entities.pk.OrderItemPk;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPk> {
     
 }
