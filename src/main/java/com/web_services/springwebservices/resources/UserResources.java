f8c2ba521786439c548129bd9a5b348645e8712f
Matheus Orestes <mherreirao@gmail.com>
2026-02-11 19:25:12 -0300

    feat: Insert and Delete user

--- src/main/java/com/web_services/springwebservices/resources/UserResources.java
+++ src/main/java/com/web_services/springwebservices/resources/UserResources.java
@@ -1,13 +1,18 @@
 package com.web_services.springwebservices.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.web_services.springwebservices.entities.User;
 import com.web_services.springwebservices.services.UserServices;
@@ -33,5 +38,17 @@ public class UserResources {
         User obj = services.findById(id);
         return ResponseEntity.ok().body(obj);
     }
-    
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = services.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        services.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/web_services/springwebservices/services/UserServices.java
+++ src/main/java/com/web_services/springwebservices/services/UserServices.java
@@ -24,4 +24,12 @@ public class UserServices{
         Optional<User> obj = repository.findById(id);
         return obj.get();
     }
+
+    public User insert(User obj){
+        return repository.save(obj);
+    }
+
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
 }
