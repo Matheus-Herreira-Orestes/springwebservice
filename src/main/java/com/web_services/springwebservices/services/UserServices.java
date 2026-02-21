51a57a1d291ea6b729ee24b929e178a3c34b0610
Matheus-Herreira-Orestes <mherreirao@gmail.com>
2026-02-21 11:37:16 -0300

    feat: get user exception handler

--- src/main/java/com/web_services/springwebservices/services/UserServices.java
+++ src/main/java/com/web_services/springwebservices/services/UserServices.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.web_services.springwebservices.repositories.UserRepository;
+import com.web_services.springwebservices.services.exceptions.ResourceNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,7 +23,7 @@ public class UserServices{
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
